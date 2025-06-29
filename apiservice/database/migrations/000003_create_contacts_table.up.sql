cdff7f32f9082334a9c9b90d7f707b3da7184867
steepermold <bkmz22092008@gmail.com>
2025-06-29 21:59:20 +0400

    fix(apiservice): add unique constraint on contacts phone field

--- apiservice/database/migrations/000003_create_contacts_table.up.sql
+++ apiservice/database/migrations/000003_create_contacts_table.up.sql
@@ -3,7 +3,7 @@ CREATE TABLE IF NOT EXISTS contacts
     id         SERIAL PRIMARY KEY,
     user_id    INT REFERENCES users (id) ON DELETE CASCADE,
     name       TEXT NOT NULL UNIQUE,
-    phone      TEXT,
+    phone      TEXT NOT NULL UNIQUE,
     created_at TIMESTAMP DEFAULT now(),
     updated_at TIMESTAMP DEFAULT now()
 );
